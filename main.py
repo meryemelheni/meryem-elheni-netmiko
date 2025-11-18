1898d3e949ccb4d8852247659feb110faf4ac163
meryem <elhenimeryem12@gmail.com>
2025-11-18 09:35:44 +0100

    Ajout de la fonction dire_salut

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-def acces_netmiko():
-Paramètres d'accès Cisco C8000V :
-sandbox-iosxr-1.cisco.com
-Login: admin
-Pass: C1sco12345
-ssh port: 22
-print("Hello, Git!")
-dire_bonjour()
+def dire_salut():
+print("Salut, Git!")
+dire_salut()
